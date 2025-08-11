26654359c51803d3dd01260deef1470b86899a39
Gamer-Guy123 <80005261+Gamer-Guy123@users.noreply.github.com>
2025-08-11 18:54:42 -0400

    Update Events.ts
    Made new event to hook into card cost calculations, for better modding support

--- src/Events.ts
+++ src/Events.ts
@@ -17,6 +17,10 @@ export type onTooltip = {
 };
 const onTooltipSource: { [name: string]: onTooltip } = {};
 
+export type onCostCalculation = {
+  (unit: IUnit, underworld: Underworld, prediction: boolean): void;
+};
+const onCostCalculationSource: { [name: string]: onCostCalculation } = {};
 
 export type onTakeDamage = {
   // Returns a possibly modified damage
@@ -99,6 +103,7 @@ export default {
   onLiquidSource,
   onKillSource,
   onTooltipSource,
+  onCostCalculationSource,
   onDeathSource,
   onMoveSource,
   onPickupSource,
